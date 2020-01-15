0a2777481b8bf559f152bde137603ba44722dc51
Matheus Luiz Barreto Pinho <matheus.pinho@azdesk1181.azi.local>
2020-01-15 19:05:51 -0300

    DesafioComponentes

---
+++ desafio/componente-desafio/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <menu-lateral />
+    <toolbar />
+    <v-content>
+      <galeria-fotos />
+    </v-content>
+    <footer-home />
+  </v-app>
+</template>
+
+<script>
+import Footer from "./components/footer";
+import GaleriaFotos from "./components/galeriaFotos";
+import menuLateral from "./components/menuLateral.vue";
+import toolbar from "./components/toolbar";
+export default {
+  components: {
+    "menu-lateral": menuLateral,
+    toolbar,
+    "galeria-fotos": GaleriaFotos,
+    "footer-home": Footer
+  }
+};
+</script>
