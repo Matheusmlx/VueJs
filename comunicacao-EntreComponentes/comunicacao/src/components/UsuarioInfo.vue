d62d1159e376e7c7af5141233ed0df2dfecee81d
Matheus Luiz Barreto Pinho <matheus.pinho@azdesk1181.azi.local>
2020-01-16 19:00:23 -0300

    Comunicacao-Entre Eventos

---
+++ comunicacao-EntreComponentes/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- // Propriedade pode ser controlada como qualquer outro atributo -->
+  <div class="componente">
+    <h2>As Informações de Usuário</h2>
+    <p>Vários detalhes...</p>
+    <p>
+      Nome do Usuario
+      <strong>{{inverterNome()}}</strong>
+    </p>
+    <button @click="reiniciarNome">Reiniciar Nome</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    //pode ser tratado como sendo uma string ou array
+    nome: [String, Array]
+  },
+  methods: {
+    inverterNome() {
+      return this.nome
+        .split("")
+        .reverse()
+        .join("");
+    },
+    reiniciarNome() {
+      this.nome = "Pedro";
+      this.$emit("nomeMudou", this.nome);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.componente {
+  flex: 1;
+  background-color: #ec485f;
+  color: #fff;
+}
+</style>
+
