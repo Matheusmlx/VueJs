7ec717f7f1dd10f53a3ff12f8cc270e4f5c51d5a
Matheus Luiz Barreto Pinho <matheus.pinho@azdesk1181.azi.local>
2020-01-17 18:41:31 -0300

    Patrimonio

---
+++ desafio/TipoBem.vue
@@ -0,0 +1,105 @@
+<template>
+  <v-form>
+    <div class="az-form-content">
+      <v-container fluid class="grey lighten-5">
+        <v-row class="lighten-5" align="center" justify="center">
+          <v-col cols="4">
+            <p style="color:grey">Selecione o tipo de bem intangivel:</p>
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-col cols="12" class="ma-3">
+            <v-row align="center" justify="center" class="lighthen-5">
+              <v-col cols="4" v-for="bem in bens" :key="bem.id">
+                <v-card
+                  outlined
+                  tile
+                  class="ma-3 pa-3"
+                  hover
+                  :style="bem.style"
+                  @click="selecionarBem(bem),bemSelecionad=true"
+                >
+                  <v-row justify="center">
+                    <v-icon :color="bem.iconStyle" large>{{bem.icon}}</v-icon>
+                  </v-row>
+                  <v-row class="mt-3" justify="center">{{bem.nome}}</v-row>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: "TipoBemEdicao",
+  data() {
+    return {
+      bemSelecionado: Boolean,
+      estiloBemSelecionado: "",
+      bens: [
+        {
+          id: 1,
+          nome: "SOFTWARES",
+          icon: "mouse",
+          style: "color:grey"
+        },
+        {
+          id: 2,
+          nome: "DIREITOS AUTORAIS",
+          icon: "copyright",
+          iconStyle: "",
+          style: "color:grey"
+        },
+        {
+          id: 3,
+          nome: "LICENÇAS",
+          icon: "colorize",
+          iconStyle: "",
+          style: "color:grey"
+        },
+        {
+          id: 4,
+          nome: "MARCAS",
+          icon: "copyright",
+          iconStyle: "",
+          style: "color:grey"
+        },
+        {
+          id: 5,
+          nome: "TÍTULOS DE PUBLICAÇÃO",
+          icon: "copyright",
+          iconStyle: "",
+          style: "color:grey"
+        },
+        {
+          id: 6,
+          nome: "RECEITAS, FÓRMULAS E PROJETOS",
+          icon: "copyright",
+          iconStyle: "",
+          style: "color:grey"
+        }
+      ]
+    };
+  },
+  methods: {
+    selecionarBem(bem) {
+      if (this.bemSelecionado) {
+        bem.style = "background:#6B8E23;color:#fff";
+        bem.iconStyle = "white";
+      } else {
+        (bem.style = "color:grey"), (bem.iconStyle = "");
+      }
+      this.bemSelecionado = !this.bemSelecionado;
+    }
+  },
+  $_veeValidate: {
+    validator: "new"
+  }
+};
+</script>
+
+<style lang="stylus"></style>
---
+++ desafio/TipoBemEdicao.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-form>
+    <div class="az-form-content">
+      <v-container fluid class="grey lighten-5">
+        <v-row justify="end">
+          <v-col cols="1">
+            <v-btn @click="novoDocumento" dark class="green text-white">Novo</v-btn>
+          </v-col>
+        </v-row>
+        <!-- Componente-Documentos -->
+        <v-row v-for="i in documentos" :key="i">
+          <v-col cols="12">
+            <v-row align="center" justify="center">
+              <v-col cols="11">
+                <v-card hover>
+                  <v-row justify="center" class="ma-3 lighten-5">
+                    <v-col cols="3">
+                      <label>Número</label>
+                      <v-text-field label="Digite o seu número" single-line outlined></v-text-field>
+                    </v-col>
+                    <v-col cols="3">
+                      <label>Data</label>
+                      <v-text-field
+                        solo
+                        label="__/__/___"
+                        mask="###.###.###-##"
+                        single-line
+                        outlined
+                        append-icon="calendar_today"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="2">
+                      <label>Valor</label>
+                      <v-text-field solo label="R$ 0,00" single-line outlined></v-text-field>
+                    </v-col>
+                    <v-col cols="2">
+                      <label>Tipo</label>
+                      <v-select required :items="items" label="Selecione"></v-select>
+                    </v-col>
+                    <v-col cols="2">
+                      <label>Anexo</label>
+                      <v-text-field solo label="Selecione o Arquivo" single-line outlined></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-card>
+              </v-col>
+              <v-col cols="1">
+                <v-btn @click="removerDocumento(i)" icon class="grey">
+                  <v-icon color="black">remove</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: "TipoBemEdicao",
+  data() {
+    return {
+      documentos: [{ id: 1 }],
+      items: ["Item 1", "Item 2", "Item 3", "Item 4"]
+    };
+  },
+  methods: {
+    novoDocumento() {
+      this.documentos.push({ id: this.documentos.length + 1 });
+      console.log(this.documentos);
+    },
+    removerDocumento(documento) {
+      this.documentos.splice(documento.id, 1);
+    }
+  },
+  $_veeValidate: {
+    validator: "new"
+  }
+};
+</script>
+
+<style lang="stylus"></style>
